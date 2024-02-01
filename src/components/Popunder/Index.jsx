d96b25930ed7321dd30a7a180abc1aea657ab685
Dipanshu-Singh-Dev <work.dipanshu.singh@gmail.comnode>
2024-02-02 01:08:55 +0530

    Completed popunder

---
+++ src/components/Popunder/Index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './styles.module.css'
+const Index = () => {
+  console.log("Popunder")
+  return (
+    <div id={styles.container}>
+      <p>Technology</p>
+      <p>Fashion</p>
+      <p>Food</p>
+      <p>Travel</p>
+      <p>Sports</p>
+      <p>Music</p>
+      <p>Art</p>
+    </div>
+  );
+}
+
+export default Index
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import styles from "./table.module.css";
 import data from "../../data";
 import { v4 as uuidv4 } from "uuid";
+import Popunder from "../Popunder/Index";
 const ResponsiveTable = () => {
-  const tags = [
-    "Technology",
-    "Fashion",
-    "Food",
-    "Travel",
-    "Sports",
-    "Music",
-    "Art",
-    "Health",
-    "Education",
-    "Finance",
-  ];
+  const [toggle,setToggle] = useState({})
+  const togglePopunder = (i) =>{
+    setToggle(prev=>{
+      console.log(prev)
+      for(let key in prev){
+        prev[key] = false;
+      }
+      prev[i] = !prev[i];
+      console.log(prev);
+      return {...prev};
+    });
+  }
+  useEffect(()=>{
+    data.map((el,index)=>{
+      toggle[index] = false;
+    })
+  },[toggle])
   return (
     <div className={styles.container}>
       <p id={styles.head}> Uploads</p>
@@ -22,59 +28,72 @@ const ResponsiveTable = () => {
         <ul className={styles.responsiveTable}>
           <li className={styles.tableHeader}>
             <p className={`${styles.col} ${styles.col1}`}>SI No.</p>
-            <p className={`${styles.col} ${styles.col2}`}>Link</p>
+            <p className={`${styles.col} ${styles.col2}`}>Links</p>
             <p className={`${styles.col} ${styles.col3}`}>Prefix</p>
             <p className={`${styles.col} ${styles.col4}`}>Tags</p>
             <p className={`${styles.col} ${styles.col5}`}>Selected Tags</p>
           </li>
-          {data.map((el) => (
-            <li key={uuidv4()} className={styles.tableRow}>
-              <p className={`${styles.col} ${styles.col1}`}>
-                {el.id < 10 ? `0${el.id}` : el.id}
-              </p>
-              <p
-                className={`${styles.col} ${styles.col2}`}
-                style={{ color: "#5B93FF", cursor: "pointer" }}
-                onClick={() => (window.location.href = `${el.link}`)}
-              >
-                {el.link.slice(8)}
-              </p>
 
-              <p className={`${styles.col} ${styles.col4}`}>{el.prefix}</p>
-              <p className={`${styles.col} ${styles.col4}`}>
-                <select>
-                  <option value="">Select Tags</option>
-                  {tags.map((el) => (
-                    <option key={uuidv4()} value={el}>
-                      {el}
-                    </option>
-                  ))}
-                </select>
-              </p>
-              <p id={styles.tags} className={`${styles.col} ${styles.col5}`}>
-                {el.tags.map((el) => (
-                  <p key={uuidv4()}>
-                    {el}{" "}
+          {data.map((el,index) => {
+            return (
+              <li key={uuidv4()} className={styles.tableRow}>
+                <p className={`${styles.col} ${styles.col1}`}>
+                  {el.id < 10 ? `0${el.id}` : el.id}
+                </p>
+                <p
+                  className={`${styles.col} ${styles.col2}`}
+                  style={{ color: "#5B93FF", cursor: "pointer" }}
+                  onClick={() => (window.location.href = `${el.link}`)}
+                >
+                  {el.link.slice(8)}
+                </p>
+
+                <p className={`${styles.col} ${styles.col4}`}>{el.prefix}</p>
+                <div style={{position:"relative"}} className={`${styles.col} ${styles.col4}`}>
+                  <p onClick={() => togglePopunder(index)}>
+                    Select a Tag
                     <svg
-                      style={{ cursor: "pointer" }}
-                      width="8"
-                      height="8"
-                      viewBox="0 0 8 8"
+                      width="25"
+                      height="24"
+                      viewBox="0 0 25 24"
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
-                        d="M1 1L4 4M4 4L1 7M4 4L7 7M4 4L7 1"
-                        stroke="white"
+                        d="M17 9.75L12.5 14.25L8 9.75"
+                        stroke="#999CA0"
                         strokeLinecap="round"
                         strokeLinejoin="round"
                       />
                     </svg>
                   </p>
-                ))}
-              </p>
-            </li>
-          ))}
+                  {toggle[index] ? <Popunder /> : null}
+                </div>
+                <p id={styles.tags} className={`${styles.col} ${styles.col5}`}>
+                  {el.tags.map((el) => (
+                    <p key={uuidv4()}>
+                      {el}{" "}
+                      <svg
+                        style={{ cursor: "pointer" }}
+                        width="8"
+                        height="8"
+                        viewBox="0 0 8 8"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M1 1L4 4M4 4L1 7M4 4L7 7M4 4L7 1"
+                          stroke="white"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                    </p>
+                  ))}
+                </p>
+              </li>
+            );
+            })}
         </ul>
       </div>
     </div>
